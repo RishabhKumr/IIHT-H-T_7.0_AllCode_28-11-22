8b040158e0489fcaad79161ebf9f9ae5fb01c9c5
unknown <kumarrishabh201@gmail.com>
2022-12-01 17:33:24 +0530

    JSHTML Promises, forms

--- HTML,JS,TS_Basics/promise.js
+++ HTML,JS,TS_Basics/promise.js
@@ -13,3 +13,32 @@ const func3 = () => {
 func1();
 func2();
 func3();
+
+
+//Promises
+
+const func4 = () =>{
+    return "this is function 4";
+}
+const func5 = ()=>{
+
+    return new Promise((resolve,reject) => {
+        setTimeout( ()=>{
+            resolve ("Hey this function was executed in set time out(promises return)")
+        },2000);
+        console.log("this is function 5");
+    })
+}
+const func6 = () => {
+    return "this is function 6";
+}
+
+const callFunc = async () =>{
+    let val1 = func4();
+    console.log(val1)
+    let val2 = await func5();
+    console.log(val2)
+    let val3 = func6();
+    console.log(val3);
+
+}
