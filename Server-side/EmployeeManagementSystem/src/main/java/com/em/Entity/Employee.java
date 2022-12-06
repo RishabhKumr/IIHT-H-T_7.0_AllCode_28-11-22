7438d1fe2b172ae2df50d8defe03f93ba1ae014f
unknown <kumarrishabh201@gmail.com>
2022-12-06 15:25:07 +0530

    SpringBoot project started

---
+++ Server-side/EmployeeManagementSystem/src/main/java/com/em/Entity/Employee.java
@@ -0,0 +1,54 @@
+package com.em.Entity;
+
+public class Employee {
+	private Integer id;
+	private String ename;
+	private String eposition;
+	private String email;
+	private Integer salary;
+	
+	public Employee(Integer id, String ename, String eposition, String email, Integer salary) {
+		super();
+		this.id = id;
+		this.ename = ename;
+		this.eposition = eposition;
+		this.email = email;
+		this.salary = salary;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getEposition() {
+		return eposition;
+	}
+	public void setEposition(String eposition) {
+		this.eposition = eposition;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Integer getSalary() {
+		return salary;
+	}
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", ename=" + ename + ", eposition=" + eposition + ", email=" + email + ", salary="
+				+ salary + "]";
+	}
+	
+}
