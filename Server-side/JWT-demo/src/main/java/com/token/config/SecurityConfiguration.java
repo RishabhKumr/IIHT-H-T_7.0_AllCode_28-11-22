02d370a0b6b313e706b36c70a1f829da53f99885
unknown <kumarrishabh201@gmail.com>
2022-12-13 02:10:26 -0800

    Spring security impl

--- Server-side/JWT-demo/src/main/java/com/token/config/SecurityConfiguration.java
+++ Server-side/JWT-demo/src/main/java/com/token/config/SecurityConfiguration.java
@@ -1,7 +1,23 @@
 package com.token.config;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfiguration {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.token.service.UserService;
+
+//@Configuration
+//@EnableWebSecurity
+@SuppressWarnings("deprecation") 
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private UserService userService;
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.userDetailsService(userService);
+		
+	}
 
 }
--- Server-side/JWT-demo/src/main/java/com/token/filter/JwtFilter.java
+++ Server-side/JWT-demo/src/main/java/com/token/filter/JwtFilter.java
@@ -13,38 +13,45 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter{
-	
-	@Autowired
-	private JWTUtility jwtUtility;
-	
-	@Autowired
-	private UserService userService;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String authorization = request.getHeader("Authorization");
-		String token = null;
-		String userName = null;
 		
-		if(null != authorization && authorization.startsWith("Bearer ")) {
-			token = authorization.substring(7);
-			userName = jwtUtility.getUsernameFromToken(token);
-		}
-		
-		if(null != userName && SecuritContextHolder.getContext().getAuthorization() == null) {
-			UserDetails userDetails = userService.loadUserByUsername(userName);
-			
-			if(jwtUtility.validateToken(token,userDetails)) {
-				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-						new UsernamePasswordAuthenticationToken(userDetails,userDetails.getAuthorities());
-				usernamePasswordAuthenticationToken.setDetails(new 
-						WebAuthenticationDetailSource().buildDetails(request));
-				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-			};
-		}
-		filterChain.doFilter(request, response);
 	}
+	
+//	@Autowired
+//	private JWTUtility jwtUtility;
+//	
+//	@Autowired
+//	private UserService userService;
+//
+//	@Override
+//	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+//			throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		String authorization = request.getHeader("Authorization");
+//		String token = null;
+//		String userName = null;
+//		
+//		if(null != authorization && authorization.startsWith("Bearer ")) {
+//			token = authorization.substring(7);
+//			userName = jwtUtility.getUsernameFromToken(token);
+//		}
+//		
+//		if(null != userName && SecuritContextHolder.getContext().getAuthorization() == null) {
+//			UserDetails userDetails = userService.loadUserByUsername(userName);
+//			
+//			if(jwtUtility.validateToken(token,userDetails)) {
+//				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+//						new UsernamePasswordAuthenticationToken(userDetails,userDetails.getAuthorities());
+//				usernamePasswordAuthenticationToken.setDetails(new 
+//						WebAuthenticationDetailSource().buildDetails(request));
+//				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+//			};
+//		}
+//		filterChain.doFilter(request, response);
+//	}
 	
 }
