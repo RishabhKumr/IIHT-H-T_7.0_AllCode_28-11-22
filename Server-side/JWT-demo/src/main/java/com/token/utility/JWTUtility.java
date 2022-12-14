4e4eaf6c56b9df85f338bc0e3b81c8770d555fbd
unknown <kumarrishabh201@gmail.com>
2022-12-14 15:26:49 +0530

    Angular app created in client side

---
+++ Server-side/JWT-demo/src/main/java/com/token/utility/JWTUtility.java
@@ -0,0 +1,61 @@
+package com.token.utility;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JWTUtility implements Serializable{
+	
+	private static final long serialVersionUID=234234252L;
+	public static final long JWT_TOKEN_VALIDITY=5*60*60;
+	public String secretKey="Rishabh";
+	
+	//to retieve username from JWT Token
+	public String getUsernameFromToken(String Token) {
+		return getClaimFromToken(token,Claims::getSubject);
+	}
+	//TO RETIEVE THE EXPIRATION DATE FROM THE TOKEN
+	public Date getExpirationDateFromToken(String token) {
+		return getClaimFromToken(token,Claims::getExpiration);
+	}
+	//
+	
+	private <T> T getClaimFromToken(String token,Function<Claims,T> claimsResolver) {
+		final Claims claims = getAllClaimsFromToken(token);
+		return claimsResolver.apply(claims);
+	}
+	
+	//for retiving any information from the token we will need for the security key
+	private Claims getAllClaimsFromTokenClaims(String token) {
+		return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+		}
+	
+	//method to check if the token is expired
+	private Boolean isTokenExpired(String token) {
+		final Date expiration = getExpirationDateFromToken(token);
+		return expiration.before(new Date());
+	}
+	
+	//generate token for user
+	public String generateToken(UserDetails userDetails) {
+		Map<String,Object> claims = new HashMap<>();
+		return doGenerateToken(claims,userDetails.getUsername());
+		
+	}
+	
+	private String doGenerateToken(Map<String,Object> claims,String subject) {
+		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY*1000))
+				.signWith(SignatureAlgorithm.HS256, secretKey).compact();
+	}
+
+}
--- Server-side/User-Microservice/src/main/java/com/user/entity/Reader.java
+++ Server-side/User-Microservice/src/main/java/com/user/entity/Reader.java
@@ -1,7 +1,11 @@
 package com.user.entity;
 
+import java.util.List;
+
 public class Reader {
 	private String readerName;
 	private String readerEmail;
 	private String readerPassword;
+	private List<Book> readerSubscribedBooks;
+	private List<Book> readerUnSubscribedBooks;
 }
