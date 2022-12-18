c19a6576a52ef50c7f9f627b5a4243d371482076
unknown <kumarrishabh201@gmail.com>
2022-12-18 08:54:59 +0530

    Added cross origin in controller

--- Server-side/Library-Management-System/src/main/java/com/lms/controller/LMSController.java
+++ Server-side/Library-Management-System/src/main/java/com/lms/controller/LMSController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -19,6 +20,7 @@ import com.lms.entity.Book;
 import com.lms.service.ILMSService;
 
 @RestController
+@CrossOrigin("http://localhost:4200/")
 public class LMSController {
 	
 	@Autowired
