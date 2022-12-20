9486a33a01f1219c52d9408382a852b701bee8ce
unknown <kumarrishabh201@gmail.com>
2022-12-21 01:17:02 +0530

    JWT Created

--- Server-side/User-Microservice/src/main/java/com/user/UserMicroserviceApplication.java
+++ Server-side/User-Microservice/src/main/java/com/user/UserMicroserviceApplication.java
@@ -2,6 +2,9 @@ package com.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class UserMicroserviceApplication {
@@ -9,5 +12,11 @@ public class UserMicroserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserMicroserviceApplication.class, args);
 	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
 
 }
