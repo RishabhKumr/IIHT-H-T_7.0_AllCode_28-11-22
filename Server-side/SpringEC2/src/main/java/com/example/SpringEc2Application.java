6a7169181d6ba6a31bc126bca50d914a950c87e8
unknown <kumarrishabh201@gmail.com>
2022-12-23 21:49:22 +0530

    AWS test

---
+++ Server-side/SpringEC2/src/main/java/com/example/SpringEc2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEc2Application.class, args);
+	}
+
+}
